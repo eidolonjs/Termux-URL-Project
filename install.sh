5ab8f6c227033757326ea5f4b21c9b5f3a086d7a
eidolonjs <76830229+eidolonjs@users.noreply.github.com>
2021-01-01 13:12:01 +0900

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Clean Install
-rm -f "~/bin/termux-url-opener"
-mkdir ~/bin
+rm -f "/data/data/com.termux/files/home/bin/termux-url-opener"
+mkdir /data/data/com.termux/files/home/bin
 #Installing required packages
 pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y
 yes | pip install youtube-dl && yes | pip install spotdl && yes | pip install youtube-dlc && yes | pip install you-get
 
 #Installing the script
-wget -P "~/bin/" --no-check-certificate "https://raw.githubusercontent.com/eidolonjs/Termux-URL-Project/main/termux-url-opener" 
-chmod +x "~/bin/termux-url-opener"
+wget -P "/data/data/com.termux/files/home/bin/" --no-check-certificate "https://raw.githubusercontent.com/eidolonjs/Termux-URL-Project/main/termux-url-opener" 
+chmod +x "/data/data/com.termux/files/home/bin/termux-url-opener"
 termux-setup-storage
